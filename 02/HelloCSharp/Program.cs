1a21f8ccd4dcf4f219c022848a9e2cde71af2909
Pushpinder <pushpinder.kaur@revature.com>
2022-12-26 15:44:03 +0530

    C# Program Structure

--- 02/HelloCSharp/Program.cs
+++ 02/HelloCSharp/Program.cs
@@ -1,10 +1,23 @@
-﻿// aingle line comment
+﻿// single line comment
 
 /*multi
 line
 comment*/
-Console.WriteLine("Please enter your name");
+/*Console.WriteLine("Please enter your name");
 string name = Console.ReadLine();
 // Console is a predefined class, WriteLine -> is a method of Console class which prints the string written inside
-Console.WriteLine($"Hello {name}!");
+Console.WriteLine($"Hello {name}!");*/
 
+//// - previous version of .Net 6 the program structure looks like below
+using System;
+namespace HelloCSharp{
+    //Types - class, struct, enums, interface, delegates
+    class Program{
+        //Type members - Eg: Method, properties, fields/variables, constructors
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine(args[0]);//Printing first command line arguement
+            User.GreetUser();
+        }
+    }
+}
