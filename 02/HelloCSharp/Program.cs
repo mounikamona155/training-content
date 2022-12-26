952df0d713efc8ddd7b27626575ed0afe775a88b
Pushpinder <pushpinder.kaur@revature.com>
2022-12-26 11:59:43 +0530

    CSharp program

---
+++ 02/HelloCSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿// aingle line comment
+
+/*multi
+line
+comment*/
+Console.WriteLine("Please enter your name");
+string name = Console.ReadLine();
+// Console is a predefined class, WriteLine -> is a method of Console class which prints the string written inside
+Console.WriteLine($"Hello {name}!");
+
