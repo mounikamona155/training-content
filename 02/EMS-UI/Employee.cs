80a816a2b650e15d14a9b1b117a0c4e30361d9f4
Pushpinder <pushpinder.kaur@revature.com>
2022-12-27 13:49:49 +0530

    method overriding

--- 02/EMS-UI/Employee.cs
+++ 02/EMS-UI/Employee.cs
@@ -2,70 +2,66 @@
 namespace EMS_UI
 {
     // Types
-    class Employee
+    partial class Employee
     {
         //int? age;//nullable integer
         //type members - variable (define state/attribute of the object)
         string firstName, lastName;
         // properties is another way to represent encapsulation
-        public string FirstName{
+        public string FirstName
+        {
             //readable accessor
-            get{
+            get
+            {
                 // + is used in strings to do string concatenation but it is inefficient way because strings are immutable, use string Builder instead
                 //if(firstName != "" || firstName != null)
-                if(String.IsNullOrEmpty(firstName))
-                    throw new InvalidDataException("The firstname is empty or null") ; 
+                if (String.IsNullOrEmpty(firstName))
+                    throw new InvalidDataException("The firstname is empty or null");
                 return firstName;
             }
             //writable accessor
             set
             {
-                if(value.Length<=0)
-                    throw new InvalidDataException("The firstname should not be empty") ;   
+                if (value.Length <= 0)
+                    throw new InvalidDataException("The firstname should not be empty");
                 firstName = value;
             }
             //to assign a new value only dueing object construction
             //init;
         }
-        public string LastName{
-            get{
-                 if(String.IsNullOrEmpty(lastName))
-                    throw new InvalidDataException("The lastname is empty") ; 
-                  return lastName;
-                
+        public string LastName
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(lastName))
+                    throw new InvalidDataException("The lastname is empty");
+                return lastName;
+
             }
-            set{
-                 if(value.Length<=0)
-                    throw new InvalidDataException("The firstname should not be empty") ;   
+            set
+            {
+                if (value.Length <= 0)
+                    throw new InvalidDataException("The firstname should not be empty");
                 lastName = value;
             }
         }
         // this is called auto-property - where it does not find any declared private variable. so implicitly it creates a private variable behind the scenes
-       public string Id { get; set; }
-       
-       //parameterlesss constructor
-       public Employee()//:this("Chanel","Lora")
-       {
-        //default values
-            Id="101";
-            firstName="Lilly";
+        public string Id { get; set; }
+
+        //parameterlesss constructor
+        public Employee()//:this("Chanel","Lora")
+        {
+            //default values
+            Id = "101";
+            firstName = "Lilly";
             lastName = "Windsor";
-       }
+        }
 
-       //parameterized constructor
-       public Employee(string firstName, string lastName){
-            this.firstName=firstName;
-            this.lastName=lastName;
-       }
-
-       internal string GetDetails(){
-            return $"Employee Id - {Id} \nEmployee name - {FirstName} {LastName}";
-       }
-       internal string GetDetails(string Id, string firstName, string lastName){
-            this.firstName=firstName;
-            this.lastName=lastName;
-            this.Id=Id;
-            return $"Employee Id - {Id} \nEmployee name - {FirstName} {LastName}";
-       }
+        //parameterized constructor
+        public Employee(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
     }
 }
--- 02/EMS-UI/Program.cs
+++ 02/EMS-UI/Program.cs
@@ -3,12 +3,33 @@ using EMS_UI;
 
 Employee emp=new Employee();//instatiang an object of the class
 
-// emp.Id ="";
-// emp.FirstName="Joe";
-// emp.LastName="Schwartz";
+emp.Id ="101";
+emp.FirstName="Joe";
+emp.LastName="Schwartz";
+emp.payRate = 50.0M;
+emp.hours = 40;
+emp.bonus = 200.0M;
+decimal addents = emp.payRate* emp.hours+emp.bonus;
+emp.taxes=0.3M * addents;
+emp.reimbursements = 25;
 //System.Console.WriteLine("Default "+ default(bool));
-Console.WriteLine(emp.GetDetails("201","Carol","Baxtor"));
+//Console.WriteLine(emp.GetDetails());
+//Console.WriteLine($"Salary - {emp.CalculateSalary()}");
 
 
-// Manager mgr=new Manager();
-// Console.WriteLine($"Employee Id - {mgr.Id} \nEmployee name - {mgr.FirstName} {mgr.LastName}");
+Manager mgr=new Manager();
+mgr.Id ="301";
+mgr.FirstName="Kaveri";
+mgr.LastName="Priya";
+mgr.payRate = 150.0M;
+mgr.hours = 40;
+mgr.bonus = 1200.0M;
+decimal MgrAddents = mgr.payRate* mgr.hours+mgr.bonus;
+mgr.taxes=0.4M * addents;
+mgr.reimbursements = 525;
+mgr.paidVacation = 1000;
+mgr.healthCare = 500;
+mgr.housingAllowance = 625;
+
+Console.WriteLine(mgr.GetDetails());
+Console.WriteLine($"Salary - ${mgr.CalculateSalary()}/month");
