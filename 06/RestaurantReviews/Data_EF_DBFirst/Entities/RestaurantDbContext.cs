b01aa23cc960d6deec1e2d0d2b0453401282f341
Pushpinder <pushpinder.kaur@revature.com>
2023-01-23 15:32:48 +0530

    Removed connection string

--- 06/RestaurantReviews/Data_EF_DBFirst/Entities/RestaurantDbContext.cs
+++ 06/RestaurantReviews/Data_EF_DBFirst/Entities/RestaurantDbContext.cs
@@ -6,6 +6,7 @@ namespace Data_EF_DBFirst.Entities;
 
 public partial class RestaurantDbContext : DbContext
 {
+    string path = File.ReadAllText("../../../connectionString.txt");
     public RestaurantDbContext()
     {
     }
@@ -23,7 +24,7 @@ public partial class RestaurantDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:pushpi-server.database.windows.net,1433;Initial Catalog=RestaurantDb; User ID=pushpinder;Password=Password123;");
+        => optionsBuilder.UseSqlServer(path);
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
