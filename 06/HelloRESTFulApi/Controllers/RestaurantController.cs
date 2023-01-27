3f97c5199825eef72a47f59898597d36d87c49e0
Pushpinder <pushpinder.kaur@revature.com>
2023-01-27 12:26:27 +0530

    Hello API

---
+++ 06/HelloRESTFulApi/Controllers/RestaurantController.cs
@@ -0,0 +1,17 @@
+﻿using HelloRESTFulApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloRESTFulApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
+    {
+        [HttpGet()]
+        public ActionResult<List<Restaurant>> Get()
+        {
+            return DummyData.GetRestaurants();
+        }
+    }
+}
---
+++ 06/HelloRESTFulApi/DummyData.cs
@@ -0,0 +1,17 @@
+﻿using HelloRESTFulApi.Models;
+
+namespace HelloRESTFulApi
+{
+    public class DummyData
+    {
+        public static List<Restaurant> GetRestaurants()
+        {
+            return new List<Restaurant>() {
+            new Restaurant(){Id=1,Name="Chipotle",Cuisine="Mexican",Zipcode="20190"},
+            new Restaurant(){Id=2,Name="Taco Bell",Cuisine="Tex Mex",Zipcode="60130"},
+            new Restaurant(){Id=3,Name="Udippi",Cuisine="South India",Zipcode="600001"},
+            new Restaurant(){Id=4,Name="Pind Baluch",Cuisine="Punjabi",Zipcode="110110" }
+            };
+        }
+    }
+}
